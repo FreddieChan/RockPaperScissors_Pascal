08970ecf9c9530b0f09462e091641a6ad2431e61
FreddieLui <luiszekwan>
2018-11-17 15:05:25 +0800

    edits

--- RockPaperScissors.pas
+++ RockPaperScissors.pas
@@ -1,5 +1,5 @@
  Var
-      player, cpu, uwincount, cwincount, playcount : Integer;
+      player, cpu, uwincount, cwincount, tiecount, playcount : Integer;
       Playerinput : String;
 
 Function winlosetie(player : Integer): String; {player}
@@ -8,27 +8,38 @@ Begin
   Randomize;
   CPU := Random(3);
   Case cpu Of
-    0: Writeln('Rock');
-    1: Writeln('Paper');
-    2: Writeln('Scissor');
+    0: Writeln('CPU: Rock');
+    1: Writeln('CPU: Paper');
+    2: Writeln('CPU: Scissor');
   End;
   If cpu = player Then
-    winlosetie := 'Tie'
+  begin
+    winlosetie := 'Tie';
+    tiecount := tiecount+1
+  end
   Else If ((player = 0) And (cpu = 1)) Or ((player = 1) And (cpu = 2)) Or ((player = 2) And (cpu = 0
           ))
          Then
-         winlosetie := 'Lose'
+         begin
+            winlosetie := 'Lose';
+            cwincount := cwincount +1;
+         end
+            
   Else If ((player = 1) And (cpu = 0)) Or ((player = 2) And (cpu = 1)) Or ((player = 0) And (cpu = 2
           ))
          Then
-         winlosetie := 'Win'
+         begin
+            winlosetie := 'Win';
+            uwincount := uwincount +1;
+         end
   Else
     Writeln('winlosetie error');
 End;
 Begin
   Writeln('welcome to the game');
+  playcount:= 1;
   Repeat
-    Writeln('Your play, Enter (R)ock, (P)aper, (S)tone. Play', playcount, '/5' );
+    Writeln('Your play, Enter (R)ock, (P)aper, (S)cissors. Play', playcount, '/5' );
     Readln(playerinput);
     If playerinput = 'R' Then
       Begin
@@ -47,11 +58,14 @@ Begin
              Writeln('Scissors');
              player := 2;
            End
-    Else Writeln('invalid');
+    Else Writeln('invalid.');
     Writeln(winlosetie(player));
     playcount := playcount + 1;
-  Until (Playcount=5);
-  Writeln('You have played', playcount,'times. Win: ', uwincount, ', Lose: ',playcount - uwincount
+  Until (Playcount=6);
+
+  Playcount := playcount -1;
+  Writeln('You have played 5 times. Win: ', uwincount, ', Lose: ', cwincount,'Tie: ',tiecount
   );
+  Readln;
   Readln;
 End.
