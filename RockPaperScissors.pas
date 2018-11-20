5e394192fc31dd0ac8b1ac3eb94567296e5803e6
FreddieLui <luiszekwan@gmail.com>
2018-11-21 02:31:54 +0800

    edits

--- RockPaperScissors.pas
+++ RockPaperScissors.pas
@@ -1,9 +1,12 @@
-uses crt;
+
+Uses crt;
+
 Var 
-    player, cpu, uwincount, cwincount, tiecount, playcount :   Integer;
+    player, cpu, uwincount, cwincount, Drawcount, playcount :   Integer;
     Playerinput :   String;
 
-Function winlosetie(player : Integer):   String; {player}{rockR0, paperP1, scissorS2}
+Function winloseDraw(player : Integer):   String; {player}
+{rockR0, paperP1, scissorS2}
 Begin
     Randomize;
     CPU := Random(3);
@@ -14,15 +17,15 @@ Begin
     End;
     If cpu = player Then
         Begin
-            winlosetie := 'Tie';
-            tiecount := tiecount+1
+            winloseDraw := 'Draw';
+            Drawcount := Drawcount+1
         End
     Else If ((player = 0) And (cpu = 1)) Or ((player = 1) And (cpu = 2)) Or ((
             player = 2) And (cpu = 0
             ))
              Then
              Begin
-                 winlosetie := 'Lose';
+                 winloseDraw := 'Lose';
                  cwincount := cwincount +1;
              End
 
@@ -31,53 +34,57 @@ Begin
             ))
              Then
              Begin
-                 winlosetie := 'Win';
+                 winloseDraw := 'Win';
                  uwincount := uwincount +1;
              End
     Else
-        Writeln('winlosetie error');
+        Writeln('winLoseDraw error');
 End;
 
-procedure center(str:String; ln:integer);
-begin
-  GotoXY((80 - length(Str)) div 2, ln);
-  write(str);
-end;
+Procedure center(str:String; ln:integer);
+Begin
+    GotoXY((80 - length(Str)) div 2, ln);
+    write(str);
+End;
 
 {main}
 Begin
-    center('welcome to the game', 1);
-    Writeln;
+    center('Welcome to the game', 1);
+    center(#201+ #205+ #205+ #205+ #205+ #205+ #205+ #205+ #205+ #205+ #205,2);
+    center('',3);{}
+    center('',4);{}
+    Delay(2000);
+    Clrscr;
     playcount := 1;
     Repeat
         Writeln('Your play, Enter (R)ock, (P)aper, (S)cissors. Play', playcount,
                 '/5' );
-        Readln(playerinput);
+        playerinput := Upcase(readkey);
         If playerinput = 'R' Then
             Begin
-                Writeln('Rock');
+                Writeln('Rock');{REPLACE WITH ASCII ART}
                 player := 0;
             End
         Else If 
-                playerinput = 'P' Then
+                playerinput = 'P'   Then
                  Begin
-                     Writeln('Paper');
+                     Writeln('Paper');{REPLACE WITH ASCII ART}
                      player := 1;
                  End
         Else If 
                 playerinput = 'S' Then
                  Begin
-                     Writeln('Scissors');
+                     Writeln('Scissors');{REPLACE WITH ASCII ART}
                      player := 2;
                  End
-        Else Writeln('invalid.');
-        Writeln(winlosetie(player));
+        Else Writeln('invalid input.');
+        Writeln(winloseDraw(player));
         playcount := playcount + 1;
     Until (Playcount=6);
 
     Playcount := playcount -1;
     Writeln('You have played 5 times. Win: ', uwincount, ', Lose: ', cwincount,
-            'Tie: ',tiecount
+            ', Draw: ',Drawcount
     );
     Readln;
     Readln;
