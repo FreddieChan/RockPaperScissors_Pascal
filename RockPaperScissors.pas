a940276ce44cea64f9b219f84107be3cff666995
AnsonYeung <s151204@tanghin.edu.hk>
2018-11-25 21:20:20 +0800

    Adjusted Programming Style

--- RockPaperScissors.pas
+++ RockPaperScissors.pas
@@ -4,130 +4,130 @@ Program RockPaperScissors;
 Uses crt;
 
 Var 
-    player, cpu, uwincount, cwincount, Drawcount, playcount : Integer;
-    Playerinput : String;
-Function winloseDraw(player : Integer):   String; {player}
+    player, cpu, uwincount, cwincount, drawCount, playCount :   Integer;
+    Playerinput :   String;
+
+Function WinLoseDraw(player : Integer) :   String;
 {rockR0, paperP1, scissorS2}
 Begin
     cursoroff;
     Randomize;
     CPU := Random(3);
-    Writeln;
+    WriteLn;
     Case cpu Of 
         0:
              Begin
-                 Writeln('CPU: ');
-                 Writeln('                _    ');
-                 Writeln('               | |   ');
-                 Writeln(' _ __ ___   ___| | __');
-                 Writeln('| |__/ _ \ / __| |/ /');
-                 Writeln('| | | (_) | (__|   < ');
-                 Writeln('|_|  \___/ \___|_|\_\');
+                 WriteLn('CPU: ');
+                 WriteLn('                _    ');
+                 WriteLn('               | |   ');
+                 WriteLn(' _ __ ___   ___| | __');
+                 WriteLn('| |__/ _ \ / __| |/ /');
+                 WriteLn('| | | (_) | (__|   < ');
+                 WriteLn('|_|  \___/ \___|_|\_\');
              End;
         1:
              Begin
-                 Writeln('CPU: ');
-                 Writeln(' _ __   __ _ _ __   ___ _ __ ');
-                 Writeln('|  _ \ / _` |  _ \ / _ \  __|');
-                 Writeln('| |_) | (_| | |_) |  __/ |   ');
-                 Writeln('| .__/ \__,_| .__/ \___|_|   ');
-                 Writeln('| |         | |              ');
-                 Writeln('|_|         |_|              ');
+                 WriteLn('CPU: ');
+                 WriteLn(' _ __   __ _ _ __   ___ _ __ ');
+                 WriteLn('|  _ \ / _` |  _ \ / _ \  __|');
+                 WriteLn('| |_) | (_| | |_) |  __/ |   ');
+                 WriteLn('| .__/ \__,_| .__/ \___|_|   ');
+                 WriteLn('| |         | |              ');
+                 WriteLn('|_|         |_|              ');
              End;
         2:
              Begin
-                 Writeln('CPU: ');
-                 Writeln('          _                        ');
-                 Writeln('         (_)                       ');
-                 Writeln(' ___  ___ _ ___ ___  ___  _ __ ___ ');
-                 Writeln('/ __|/ __| / __/ __|/ _ \|  __/ __|');
-                 Writeln('\__ \ (__| \__ \__ \ (_) | |  \__ \');
-                 Writeln('|___/\___|_|___/___/\___/|_|  |___/');
+                 WriteLn('CPU: ');
+                 WriteLn('          _                        ');
+                 WriteLn('         (_)                       ');
+                 WriteLn(' ___  ___ _ ___ ___  ___  _ __ ___ ');
+                 WriteLn('/ __|/ __| / __/ __|/ _ \|  __/ __|');
+                 WriteLn('\__ \ (__| \__ \__ \ (_) | |  \__ \');
+                 WriteLn('|___/\___|_|___/___/\___/|_|  |___/');
              End;
     End;
-    Writeln;
+    WriteLn;
     If cpu = player Then
         Begin
-            winloseDraw := 'Draw!';
-            Drawcount := Drawcount+1
+            WinLoseDraw := 'Draw!';
+            drawCount := drawCount+1
         End
     Else If (player = 0) And (cpu = 1) Or (player = 1) And (cpu = 2) Or (
             player = 2) And (cpu = 0) Then
              Begin
-                 winloseDraw := 'Lose!';
+                 WinLoseDraw := 'Lose!';
                  cwincount := cwincount +1;
              End
     Else If (player = 1) And (cpu = 0) Or (player = 2) And (cpu = 1) Or (
             player = 0) And (cpu = 2) Then
              Begin
-                 winloseDraw := 'Win!';
+                 WinLoseDraw := 'Win!';
                  uwincount := uwincount +1;
              End
     Else
-        Writeln('winLoseDraw error');
+        WriteLn('WinLoseDraw error');
 End;
-Procedure center(Str:String; Ln:Integer);
+Procedure center(Str : String; Ln : Integer);
 Begin
-    Gotoxy((80 - Length(Str)) div 2, Ln);
+    GoToXY((80 - Length(Str)) div 2, Ln);
     Write(Str);
 End;
-{main}
+
+(* Main *)
 Begin
     center('Welcome to the game', 1);
-    center('>Press any key to play.',2);
+    center('>Press any key to play.', 2);
     Readkey;
     Clrscr;
-    playcount := 1;
+    playCount := 1;
     Repeat
-        Writeln;
-        Writeln('Enter (R)ock, (P)aper, (S)cissors. Play', playcount,
+        WriteLn;
+        WriteLn('Enter (R)ock, (P)aper, (S)cissors. Play', playCount,
                 '/5' );
         playerinput := Upcase(Readkey);
- {}
-        If (playerinput ='P' ) Or (playerinput ='S' ) Or (playerinput = 'R')Then
+        If (playerinput = 'P') Or (playerinput = 'S') Or (playerinput = 'R')
+            Then
             Begin
-                Writeln('Your play:');
+                WriteLn('Your play:');
                 If playerinput = 'R' Then
                     Begin
-                        Writeln('                _    ');
-                        Writeln('               | |   ');
-                        Writeln(' _ __ ___   ___| | __');
-                        Writeln('| |__/ _ \ / __| |/ /');
-                        Writeln('| | | (_) | (__|   < ');
-                        Writeln('|_|  \___/ \___|_|\_\');
+                        WriteLn('                _    ');
+                        WriteLn('               | |   ');
+                        WriteLn(' _ __ ___   ___| | __');
+                        WriteLn('| |__/ _ \ / __| |/ /');
+                        WriteLn('| | | (_) | (__|   < ');
+                        WriteLn('|_|  \___/ \___|_|\_\');
                         player := 0;
                     End
-                Else If 
-                        playerinput = 'P' Then
+                Else If playerinput = 'P' Then
                          Begin
-                             Writeln(' _ __   __ _ _ __   ___ _ __ ');
-                             Writeln('|  _ \ / _` |  _ \ / _ \  __|');
-                             Writeln('| |_) | (_| | |_) |  __/ |   ');
-                             Writeln('| .__/ \__,_| .__/ \___|_|   ');
-                             Writeln('| |         | |              ');
-                             Writeln('|_|         |_|              ');
+                             WriteLn(' _ __   __ _ _ __   ___ _ __ ');
+                             WriteLn('|  _ \ / _` |  _ \ / _ \  __|');
+                             WriteLn('| |_) | (_| | |_) |  __/ |   ');
+                             WriteLn('| .__/ \__,_| .__/ \___|_|   ');
+                             WriteLn('| |         | |              ');
+                             WriteLn('|_|         |_|              ');
                              player := 1;
                          End
-                Else If 
-                        playerinput = 'S' Then
+                Else If playerinput = 'S' Then
                          Begin
-                             Writeln('          _                        ');
-                             Writeln('         (_)                       ');
-                             Writeln(' ___  ___ _ ___ ___  ___  _ __ ___ ');
-                             Writeln('/ __|/ __| / __/ __|/ _ \|  __/ __|');
-                             Writeln('\__ \ (__| \__ \__ \ (_) | |  \__ \');
-                             Writeln('|___/\___|_|___/___/\___/|_|  |___/');
+                             WriteLn('          _                        ');
+                             WriteLn('         (_)                       ');
+                             WriteLn(' ___  ___ _ ___ ___  ___  _ __ ___ ');
+                             WriteLn('/ __|/ __| / __/ __|/ _ \|  __/ __|');
+                             WriteLn('\__ \ (__| \__ \__ \ (_) | |  \__ \');
+                             WriteLn('|___/\___|_|___/___/\___/|_|  |___/');
                              player := 2;
                          End;
-                Writeln(winloseDraw(player));
-                playcount := playcount + 1;
+                WriteLn(WinLoseDraw(player));
+                playCount := playCount + 1;
             End
-        Else Writeln('Try again');
-    Until (Playcount=6);
-    Playcount := playcount -1;
+        Else WriteLn('Try again');
+    Until playCount = 6;
+    (* playCount := playCount - 1; *)
     WriteLn;
-    Writeln('You have played 5 times. Win: ', uwincount, ', Lose: ', cwincount,
-            ', Draw: ', Drawcount);
+    WriteLn('You have played 5 times. Win: ', uwincount, ', Lose: ', cwincount,
+            ', Draw: ', drawCount);
     WriteLn('Press any key to exit...');
     ReadKey;
 End.
